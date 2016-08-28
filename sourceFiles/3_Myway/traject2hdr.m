4f808ab6c4025e934dc2553a8cda38477b14649d
WataruOhnishi <w.ohnishi.705@gmail.com>
2016-08-27 23:51:06 -0700

    add format_name & name of #define changed
    - default: 'far float'
    possible cases: 'far int', 'far double', 'double' etc.
    
    - nrofs_name = ['NROFS_' array_name];

--- sourceFiles/3_Myway/traject2hdr.m
+++ sourceFiles/3_Myway/traject2hdr.m
@@ -1,10 +1,10 @@
-function [path] = traject2hdr(signal,folder,file,array_name)
 %REF2HDR Export reference vector to c# myway header file.
 %   [path] = traject2hdr(signal, folder, file, array_name)
 % signal    : reference vector
-% folder    : subfolder from current path 
+% folder    : subfolder from current path
 % file      : name of header file, required format: R...ref.h
-% array_name: name of array in C-code (optional) : refvec[NROF]
+% format_name: format name (optional), default: 'far float'
+% array_name: name of array in C-code (optional), default : refvec[NROF]
 % Algorithm : low level file I/O to create header for
 %             MyWay PE-Expert3 system (MWPE-C6713A DSP)
 % Author    : Thomas Beauduin, University of Tokyo
@@ -12,8 +12,16 @@ function [path] = traject2hdr(signal,folder,file,array_name)
 %%%%%
 
 if nargin < 4
-    array_name = 'refvec';
+    format_name = 'far float';
 end
+
+if nargin < 5
+    array_name = 'refvec';
+    nrofs_name = 'NROFS';
+else
+    nrofs_name = ['NROFS_' array_name];
+end
+
 
 nrofs = size(signal,1);
 
@@ -22,19 +30,22 @@ fid = fopen(strcat(folder,file),'w'); % file open
 %array_name = strrep(name,'R','');
 %nrofs_name = upper(strrep(array_name,'ref',''));
 %array_name = 'refvec';
-nrofs_name = 'NROFS';
 fprintf(fid,'#define %s %d \n', nrofs_name, nrofs);
-fprintf(fid,'far float %s [%s] = { \n',array_name,nrofs_name);
+fprintf(fid,[format_name ' %s [%s] = { \n'],array_name,nrofs_name);
 
 j = 0;
 for i=1 : nrofs
-    fprintf(fid, '%.10f, ', signal(i));
+    if isempty(strfind(format_name,'int')) == 1
+        fprintf(fid, '%.10f, ', signal(i));
+    else
+        fprintf(fid, '%d, ', round(signal(i)));
+    end
     j=j+1;
     if j == 10
         fprintf(fid, '\n'); j = 0;
     end
 end
-fprintf(fid,'}; \n'); 
+fprintf(fid,'}; \n');
 
 fclose(fid); %file closed
 
