2306a4df55359f44f2632ad9c2555d5b2cd1760e
Thomas Beauduin <thomasbeauduin@gmail.com>
2016-10-25 22:17:55 +0900

    Revert "multiple file process & bugfix"
    This reverts commit f000ca0149097c5d5802eae426f1b441ea077fce.

--- sourceFiles/3_Myway/myway2mat.m
+++ sourceFiles/3_Myway/myway2mat.m
@@ -18,77 +18,42 @@ W=[]; T=[]; R=[]; P=[]; A=[];
 % WAVE MSR DATA
 % extract wave data file from current folder (W*.csv)
 wcsv = dir(strcat(pwd,'\','W*.csv'));
-NROFF = length(wcsv);
-if NROFF ~= 0
-    for kk = 1:1:NROFF
-        [W,mname] = mwcsv2struct(strcat(pwd,'\',wcsv(kk).name),fs_dsp,cname);
-        % MERGE DATA
-        % concatenate all structures into single file (D*.mat)
-        S = mergestruct(W,T,R,P,A);
-        save(mname,'-struct','S');
-        fprintf('%s',mname);
-    end
+if exist(wcsv.name,'file') ~= 0
+    [W,mname] = mwcsv2struct(strcat(pwd,'\',wcsv.name),fs_dsp,cname);
 end
 
 % TRC MSR DATA
 % extract trace data file from current folder (T*.csv)
 tcsv = dir(strcat(pwd,'\','T*.csv'));
-NROFF = length(tcsv);
-if NROFF ~= 0
-    for kk = 1:1:NROFF
-        [T,tname] = mwtrc2struct(strcat(pwd,'\',tcsv(kk).name));
-        % MERGE DATA
-        % concatenate all structures into single file (D*.mat)
-        S = mergestruct(W,T,R,P,A);
-        save(mname,'-struct','S');
-        fprintf('%s',mname);
-    end
+if exist(tcsv.name,'file') ~= 0
+    [T,tname] = mwtrc2struct(strcat(pwd,'\',tcsv.name));
 end
 
 % REF HDR DATA
 % extract reference data file from current folder (R*.hdr)
 rhdr = dir(strcat(pwd,'\','R*.h'));
-NROFF = length(rhdr);
-if NROFF ~= 0
-    for kk = 1:1:NROFF
-        [R,rname] = mwref2struct(strcat(pwd,'\',rhdr(kk).name));
-        % MERGE DATA
-        % concatenate all structures into single file (D*.mat)
-        S = mergestruct(W,T,R,P,A);
-        save(mname,'-struct','S');
-        fprintf('%s',mname);
-    end
+if exist(rhdr.name,'file') ~= 0
+    [R,rname] = mwref2struct(strcat(pwd,'\',rhdr.name));
 end
 
 % EXP PAR DATA
 % extract parameters data file from current folder (P*.mat)
 pmat = dir(strcat(pwd,'\','P*.mat'));
-NROFF = length(pmat);
-if NROFF ~= 0
-    for kk = 1:1:NROFF
-        P = load(strcat(pwd,'\',pmat(kk).name));
-        % MERGE DATA
-        % concatenate all structures into single file (D*.mat)
-        S = mergestruct(W,T,R,P,A);
-        save(mname,'-struct','S');
-        fprintf('%s',mname);
-    end
+if exist(pmat.name,'file') ~= 0
+    P = load(strcat(pwd,'\',pmat.name));
 end
 
 % ACC MSR DATA
 % extract nvgate data file from current folder (A*.txt)
 atxt =  dir(strcat(pwd,'\','A*.txt'));
-NROFF = length(atxt);
-if NROFF ~= 0
-    for kk = 1:1:NROFF
-        [A,anm] = nvacc2struct(strcat(pwd,'\',atxt(kk).name));
-        % MERGE DATA
-        % concatenate all structures into single file (D*.mat)
-        S = mergestruct(W,T,R,P,A);
-        save(mname,'-struct','S');
-        fprintf('%s',mname);
-    end
+if exist(atxt.name,'file') ~= 0
+    [A,anm] = nvacc2struct(strcat(pwd,'\',atxt.name));
 end
+
+% MERGE DATA
+% concatenate all structures into single file (D*.mat)
+S = mergestruct(W,T,R,P,A);
+save(mname,'-struct','S');
 
 end
 
