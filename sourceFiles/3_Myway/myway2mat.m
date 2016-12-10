5e929ac21f243b1075b36df57f58be6a5aa1cea1
Thomas Beauduin <thomasbeauduin@gmail.com>
2016-12-10 21:24:39 +0900

    bugfix: mac slash operator
    acqtools now work for macOS too

--- sourceFiles/3_Myway/myway2mat.m
+++ sourceFiles/3_Myway/myway2mat.m
@@ -17,37 +17,37 @@ end
 W=[]; T=[]; R=[]; P=[]; A=[];
 % WAVE MSR DATA
 % extract wave data file from current folder (W*.csv)
-wcsv = dir(strcat(pwd,'\','W*.csv'));
+wcsv = dir(strcat(pwd,'/','W*.csv'));
 if exist(wcsv.name,'file') ~= 0
-    [W,mname] = mwcsv2struct(strcat(pwd,'\',wcsv.name),fs_dsp,cname);
+    [W,mname] = mwcsv2struct(strcat(pwd,'/',wcsv.name),fs_dsp,cname);
 end
 
 % TRC MSR DATA
 % extract trace data file from current folder (T*.csv)
-tcsv = dir(strcat(pwd,'\','T*.csv'));
+tcsv = dir(strcat(pwd,'/','T*.csv'));
 if exist(tcsv.name,'file') ~= 0
-    [T,tname] = mwtrc2struct(strcat(pwd,'\',tcsv.name));
+    [T,tname] = mwtrc2struct(strcat(pwd,'/',tcsv.name));
 end
 
 % REF HDR DATA
 % extract reference data file from current folder (R*.hdr)
-rhdr = dir(strcat(pwd,'\','R*.h'));
+rhdr = dir(strcat(pwd,'/','R*.h'));
 if exist(rhdr.name,'file') ~= 0
-    [R,rname] = mwref2struct(strcat(pwd,'\',rhdr.name));
+    [R,rname] = mwref2struct(strcat(pwd,'/',rhdr.name));
 end
 
 % EXP PAR DATA
 % extract parameters data file from current folder (P*.mat)
-pmat = dir(strcat(pwd,'\','P*.mat'));
+pmat = dir(strcat(pwd,'/','P*.mat'));
 if exist(pmat.name,'file') ~= 0
-    P = load(strcat(pwd,'\',pmat.name));
+    P = load(strcat(pwd,'/',pmat.name));
 end
 
 % ACC MSR DATA
 % extract nvgate data file from current folder (A*.txt)
-atxt =  dir(strcat(pwd,'\','A*.txt'));
+atxt =  dir(strcat(pwd,'/','A*.txt'));
 if exist(atxt.name,'file') ~= 0
-    [A,anm] = nvacc2struct(strcat(pwd,'\',atxt.name));
+    [A,anm] = nvacc2struct(strcat(pwd,'/',atxt.name));
 end
 
 % MERGE DATA
