52d469b9b00b2f45274a1ea2d5a360c59b86b17c
Thomas Beauduin <thomasbeauduin@gmail.com>
2017-05-26 15:20:05 -0700

    warning for duplicate fieldnames added

--- sourceFiles/A_Auxiliary/mergestruct.m
+++ sourceFiles/A_Auxiliary/mergestruct.m
@@ -6,15 +6,13 @@ function Y = mergestruct(varargin)
 % note  : if a fieldname is not unique among structures, 
 %         only value from last structure with this field is used.
 % author: Thomas Beauduin, University of Tokyo, 2016
-
-data_c = []; 
-data_n = [];
+%%%%%
+data_c = []; data_n = [];
 nrofs = 0;
-
-for ii=1:nargin,
+for ii=1:nargin
     X = varargin{ii};
-    if ~isempty(X),
-        if ~isstruct(X),
+    if ~isempty(X)
+        if ~isstruct(X)
             error('catstruct:InvalidArgument',...
                  ['Argument #' num2str(ii) ' is not a structure.']) ;
         else
@@ -25,22 +23,18 @@ for ii=1:nargin,
     end
 end
 
-% improve by printing the name of the repeated arrays
 if nrofs > 1
     [data_nu,idx1,idx2] = unique(data_n,'last');
     idx_rep = setdiff(idx2,idx1);
-    if numel(data_nu) ~= numel(data_n),
-        data_c = data_c(idx1,:);
-        data_n = data_n(idx1,:);
-        %for i=1:length(idx_rep)
-        %    test = data_n{idx_rep(i)}
-            %isa(test,'array')
-            %fprintf();
-            %fprintf(strcat('Warning: repeated fieldnames:\n\t\t',...
-            %                data_n(idx_rep),...
-            %               'so probably data lost in mergestruct. \n'));
-        %end
+    if numel(data_nu) ~= numel(data_n)
+        fprintf('Warning: repeated fieldnames (');
+        for i=1:length(idx_rep)
+            fprintf(strcat(data_n{idx_rep(i)},', '));
+        end
+        fprintf(') \n')
     end
+    data_c = data_c(idx1,:);
+    data_n = data_n(idx1,:);
 end
 Y = cell2struct(data_c,data_n,1);
 
