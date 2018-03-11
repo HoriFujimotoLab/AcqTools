dfdd2c51557cacc846d55f33b3037e5b7b29a663
WataruOhnishi <WataruOhnishi@users.noreply.github.com>
2018-03-11 12:03:16 +0900

    bugfix: specify exist file type

--- src/3_Myway/myway2mat.m
+++ src/3_Myway/myway2mat.m
@@ -18,35 +18,35 @@ W=[]; T=[]; R=[]; P=[]; A=[];
 % WAVE MSR DATA
 % extract wave data file from current folder (W*.csv)
 wcsv = dir(strcat(pwd,'/','W*.csv'));
-if exist(wcsv.name,'file') ~= 0
+if exist(wcsv.name,'file') == 2
     [W,mname] = mwcsv2struct(strcat(pwd,'/',wcsv.name),fs_dsp,cname);
 end
 
 % TRC MSR DATA
 % extract trace data file from current folder (T*.csv)
 tcsv = dir(strcat(pwd,'/','T*.csv'));
-if exist(tcsv.name,'file') ~= 0
+if exist(tcsv.name,'file') == 2
     [T,tname] = mwtrc2struct(strcat(pwd,'/',tcsv.name));
 end
 
 % REF HDR DATA
 % extract reference data file from current folder (R*.hdr)
 rhdr = dir(strcat(pwd,'/','R*.h'));
-if exist(rhdr.name,'file') ~= 0
+if exist(rhdr.name,'file') == 2
     [R,rname] = mwref2struct(strcat(pwd,'/',rhdr.name));
 end
 
 % EXP PAR DATA
 % extract parameters data file from current folder (P*.mat)
 pmat = dir(strcat(pwd,'/','P*.mat'));
-if exist(pmat.name,'file') ~= 0
+if exist(pmat.name,'file') == 2
     P = load(strcat(pwd,'/',pmat.name));
 end
 
 % ACC MSR DATA
 % extract nvgate data file from current folder (A*.txt)
 atxt =  dir(strcat(pwd,'/','A*.txt'));
-if exist(atxt.name,'file') ~= 0
+if exist(atxt.name,'file') == 2
     [A,anm] = nvacc2struct(strcat(pwd,'/',atxt.name));
 end
 
